f96f854c3f6f74148fc934c093c435e4d76de7bf
terut <terut.dev+github@gmail.com>
2011-11-17 01:17:05 +0900

    Make deloy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+## move current for .sh.
+cd $(dirname $0)
+
+## except .git .gitignore .gitmodules .profile .vimperatorrc README.md deploy.sh specs.watchr
+dotfiles=('.bashrc' '.gemrc' '.irbrc' '.tmux.conf' '.tmux.ext' '.vim' '.vimrc' '.zprofile' '.zshrc' 'Rakefile')
+
+## all file version.
+# for dotfile in .?*
+for dotfile in ${dotfiles}
+do
+    ## all file version.
+    # if [ $dotfile != '..' ] && [ $dotfile != '.git' ]
+    # then
+    ln -is "$PWD/$dotfile" $HOME
+    # fi
+done
