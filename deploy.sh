61d8db7411c9e7b30120f7c40159d9aeb17c985c
terut <terut.dev+github@gmail.com>
2011-11-17 01:22:07 +0900

    Add mouse.conf for Ubuntu.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 ## except .git .gitignore .gitmodules .profile .vimperatorrc README.md deploy.sh specs.watchr
-dotfiles=('.bashrc' '.gemrc' '.irbrc' '.tmux.conf' '.tmux.ext' '.vim' '.vimrc' '.zprofile' '.zshrc' 'Rakefile')
+dotfiles=('.bashrc' '.gemrc' '.irbrc' '.tmux.conf' '.tmux.ext' '.vim' '.vimrc' '.zprofile' '.zshrc' 'Rakefile' '20-middle-mouse-button.conf')
 
 ## all file version.
 # for dotfile in .?*
